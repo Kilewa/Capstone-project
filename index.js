ffcedf0b3bfe902333ace37572e96a1d0693881c
kilewa <kilewageorge230@gmail.com>
2020-06-23 02:03:28 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,26 @@
+const navSlide = () => {
+    const burger = document.querySelector('.burger');
+    const nav = document.querySelector('.nav-links');
+    const navLinks = document.querySelectorAll('.nav-links li');
+    // toggle Nav
+    burger.addEventListener('click', ()=>{
+        nav.classList.toggle('nav-active');
+       
+        //Animate links
+        navLinks.forEach((link, index) => {
+            if (link.style.animation){
+                link.style.animation = '';
+            }
+            else{
+                link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.5}s`;
+            }
+    
+  
+        });
+
+        //toggle animation
+        burger.classList.toggle('toggle');
+    });
+    
+}
+navSlide();
