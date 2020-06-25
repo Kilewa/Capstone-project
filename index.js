929eb5ef4506ee4ba1ee205a0831030e12f71f65
kilewa <kilewageorge230@gmail.com>
2020-06-26 00:27:12 +0300

    update AOS

--- index.js
+++ index.js
@@ -33,15 +33,15 @@ caretScrollToTop.addEventListener('click', function(){
     //method 1
     // window.scrollTo(0,0);
     //method 2 not compatible to all browsers
-    window.scrollTo({
-        top: 0,
-        left: 0,
-        behavior:'smooth'
+    // window.scrollTo({
+    //     top: 0,
+    //     left: 0,
+    //     behavior:'smooth'
     
 
     //method compatimble to almost all browsers 
 
     // });
-    // $(`html, body`).animate({scrollTop: 0}, 'slow');
+    $(`html, body`).animate({scrollTop: 0}, 'slow');
 });
 caretScrollToTop ();
